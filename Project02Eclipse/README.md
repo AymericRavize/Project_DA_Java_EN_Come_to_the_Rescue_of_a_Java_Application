02889d73ede5899623d0bcaac484e0b84b970421
AymericRavize <aymeric.ravize@gmail.com>
2020-10-07 21:59:46 +0200

    Projet correction exceptions

--- Project02Eclipse/README.md
+++ Project02Eclipse/README.md
@@ -1,1 +1,7 @@
 Sample project for OpenClassroom's fundamentals of Java
+
+Java 1.8
+
+1. Installer  le logiciel eclipse 
+2. Ouvrir le projet (File->open project)
+3. Cliquer sur l'icone play seul pour exécuter.
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,9 +1,11 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.ReadOnlyFileSystemException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -12,7 +14,8 @@ import java.util.Map;
 
 /**
  * 
- * ReadSymptomDataFromFile is an utilitarian class that generate a file containing the symptoms and their iteration's number
+ * ReadSymptomDataFromFile is an utilitarian class that generate a file
+ * containing the symptoms and their iteration's number
  * 
  * @author Caroline ,Ravizé Aymeric
  * @version V1.1
@@ -20,7 +23,6 @@ import java.util.Map;
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
-	
 	/**
 	 * 
 	 * @author Ravizé Aymeric
@@ -28,72 +30,78 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 	 * @since V1.1
 	 * 
 	 * @param filepath is the destination of the file that we want to read.
-	 * @return a HashMap containing the list of the symptoms and their occurence number.     
-	 *              
+	 * @return a HashMap containing the list of the symptoms and their occurence
+	 *         number.
+	 * 
 	 */
 	public static HashMap<String, Integer> GetSymptoms(String filepath) {
 
 		HashMap<String, Integer> symptoms = new HashMap<String, Integer>();
 
 		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader(new FileReader(filepath));
+			// in 1.7 java and after the declared resources are implicitly closed.
+			try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
 				// read first line of document
 				String line = reader.readLine();
 
 				while (line != null) {
-					
-					if(symptoms.containsKey(line)) {
-						//if symptom exists in HashMap increment counter
+
+					if (symptoms.containsKey(line)) {
+						// if symptom exists in HashMap increment counter
 						symptoms.replace(line, (int) symptoms.get(line) + 1);
-					}
-					else {
-						//if symptom not exists in HashMap set counter to 1
+					} else {
+						// if symptom not exists in HashMap set counter to 1
 						symptoms.put(line, 1);
 					}
 					// read next line
 					line = reader.readLine();
 				}
 				reader.close();
+			} catch (FileNotFoundException e) {
+				System.out.println("Error : file not found");
 			} catch (IOException e) {
-				e.printStackTrace();
+				System.out.println("Error : file not compliant");
 			}
 
 		}
 		return symptoms;
 	}
+
 	/**
 	 * 
 	 * @author Ravizé Aymeric
 	 * @version V1.0
 	 * @since V1.1
 	 * 
-	 * @param symptoms is a HashMap of the symptoms to read
-	 * @param newFilePath is the name of the new file that we want to create and complete
-	 *                  
+	 * @param symptoms    is a HashMap of the symptoms to read
+	 * @param newFilePath is the name of the new file that we want to create and
+	 *                    complete
+	 * 
 	 */
-	public static void WriteSymptoms(HashMap<String, Integer> symptoms , String newFilePath) {
+	public static void WriteSymptoms(HashMap<String, Integer> symptoms, String newFilePath) {
 		// sort of Hashmap when creating the iterator
 		Iterator it = symptoms.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByKey()).iterator();
-		
-		try {
+		// in 1.7 java and after the declared resources are implicitly closed.
 		// create the output file
-			FileWriter writer = new FileWriter (newFilePath);
+		try (FileWriter writer = new FileWriter(newFilePath)) {
+
 			writer.write("");
 			while (it.hasNext()) {
-				//getting the next item
+				// getting the next item
 				Map.Entry m = (Map.Entry) it.next();
 				writer.append(m.getKey() + ":" + m.getValue() + "\n");
 
 			}
 			writer.close();
+		} catch (ReadOnlyFileSystemException e) {
+			System.out.println("Error : file read-only");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+			System.out.println("Error : file not compliant");
 		}
-	
-		
+
 	}
+
 	/**
 	 * 
 	 * @author Ravizé Aymeric
@@ -101,13 +109,15 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 	 * @since V1.1
 	 * 
 	 * @param oldFilePath is the file to read destination.
-	 * @param newFilePath is the name of the new file that we want to create and complete.
-	 * @see WriteSymptoms calls GetSymptoms with the parameter oldFilePath and executes WriteSymptoms(HashMap,String) with the value that it send back and the parameter newFilePath.
-	 *                  
+	 * @param newFilePath is the name of the new file that we want to create and
+	 *                    complete.
+	 * @see WriteSymptoms calls GetSymptoms with the parameter oldFilePath and
+	 *      executes WriteSymptoms(HashMap,String) with the value that it send back
+	 *      and the parameter newFilePath.
+	 * 
 	 */
-	public static void WriteSymptoms(String oldFilePath ,String newFilePath) {
-		WriteSymptoms(GetSymptoms(oldFilePath),newFilePath);		
+	public static void WriteSymptoms(String oldFilePath, String newFilePath) {
+		WriteSymptoms(GetSymptoms(oldFilePath), newFilePath);
 	}
-
 
 }
